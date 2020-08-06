95589a25d0955b022c9d208955c4e4d29fccb9ab
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-08-06 08:24:34 -0600

    Remove IEA turbine model, update paths

--- run_ROSCOtutorial.py
+++ run_ROSCOtutorial.py
@@ -4,36 +4,36 @@ All of these abilities are also shown in the Examples folder of the ROSCO toolbo
 '''
 
 import os
+import platform
 import yaml
 from ROSCO_toolbox import utilities, turbine, controller
 
 
-# Local definitions
-openfast_call   = 'openfast_master' # Command line call to run openfast or path to openfast (e.g. '/Users/nabbas/Documents/openfast/install/bin/openfast')
-dev_branch      = True              # Are you on the dev branch of OpenFAST?         
-
-
-## -------------------------------------------- ##
-#   Nothing should need to change from here on  ##
-## -------------------------------------------- ##
+# Command line call to run openfast or path to openfast 
+if platform.system() == 'Windows':
+    openfast_call = os.path.join( os.path.dirname(os.path.realpath(__file__)), 'OpenFAST_executables', 'openfast.exe')
+elif platform.system() == 'Darwin':
+    openfast_call = os.path.join( os.path.dirname(os.path.realpath(__file__)), 'OpenFAST_executables', 'openfast_mac')
+else:
+    openfast_call = os.path.join( os.path.dirname(os.path.realpath(__file__)), 'OpenFAST_executables', 'openfast')
 
 # Define model paths
-model_path = os.path.join(os.getcwd(), 'OpenFAST', 'IEA-15-240-RWT-Monopile')
-model_fst = 'IEA-15-240-RWT-Monopile.fst'
-rotperf_file = os.path.join(os.getcwd(), 'IEA-15-240-RWT', 'OpenFAST',
-                            'IEA-15-240-RWT', 'Cp_Ct_Cq.IEA15MW.txt')
+# model_path = os.path.join(os.getcwd(), ROSCO_tutorial/IEA-15-240-RWT-Monopile)
+# model_fst = 'IEA-15-240-RWT-Monopile.fst'
+# rotperf_file = os.path.join(os.getcwd(), 'IEA-15-240-RWT', 'OpenFAST',
+#                             'IEA-15-240-RWT', 'Cp_Ct_Cq.IEA15MW.txt')
 
 # Load yaml file
-parameter_filename = os.path.join(model_path, 'ServoData', 'IEA15MW-Monopile.yaml')
+parameter_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BAR_00', 'ServoData', 'BAR_00.yaml')
 inps = yaml.safe_load(open(parameter_filename))
 path_params = inps['path_params']
 turbine_params = inps['turbine_params']
 controller_params = inps['controller_params']
 
 # Update path parameters for this tutorial
-path_params['FAST_directory'] = model_path
-path_params['FAST_InputFile'] = model_fst
-path_params['rotor_performance_filename'] = rotperf_file
+# path_params['FAST_directory'] = model_path
+# path_params['FAST_InputFile'] = model_fst
+# path_params['rotor_performance_filename'] = rotperf_file
 
 # Instantiate turbine, controller, and file processing classes
 turb = turbine.Turbine(turbine_params)
@@ -42,8 +42,7 @@ file_processing = utilities.FileProcessing()
 fast_io = utilities.FAST_IO()
 
 # Load turbine data from OpenFAST and rotor performance text file
-turb.load_from_fast(path_params['FAST_InputFile'], path_params['FAST_directory'], rot_source='txt',
-                    txt_filename=path_params['rotor_performance_filename'], dev_branch=dev_branch)
+turb.load_from_fast(path_params['FAST_InputFile'], path_params['FAST_directory'], dev_branch=True)
 
 # Tune controller
 cont.tune_controller(turb)
